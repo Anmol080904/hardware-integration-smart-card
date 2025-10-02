56a29fd5df1cca5366c976e6eff2b429222399e6
Anmol080904 <anmolchudasma1404@gmail.com>
2025-10-02 19:51:24 +0530

    `Refactored code to use TinyGPSPlus and HTTPClient libraries, updated WiFi and GPS serial configurations.`

--- sketch_aug28a.ino
+++ sketch_aug28a.ino
@@ -1,60 +1,68 @@
-#include <TinyGPS++.h>
-#include <HardwareSerial.h>
 #include <WiFi.h>
-// ==================================================================
-//                    HARDWARE CONFIGURATION
-// ==================================================================
-HardwareSerial gpsSerial(1);  
-#define GPS_RX 16             
-#define GPS_TX 17             
-#define GPS_BAUD 9600         
-const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PASSWORD";
-TinyGPSPlus gps;  // GPS parser object
+#include <HTTPClient.h>
+#include <TinyGPSPlus.h>
 
-// ==================================================================
-//                    INITIALIZATION SETUP
-// ==================================================================
+// Create GPS object
+TinyGPSPlus gps;
+
+// GPS serial pins
+#define RXD2 16
+#define TXD2 17
+
+// WiFi credentials (specific network only)
+const char* ssid = "YourWiFiName";
+const char* password = "YourWiFiPassword";
+
+// Dummy API endpoint
+String serverName = "http://jsonplaceholder.typicode.com/posts"; 
+
+// Hardware serial for GPS
+HardwareSerial neogps(1);
+
 void setup() {
   Serial.begin(115200);
-  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, GPS_RX, GPS_TX);
-  Serial.println("Connecting to WiFi...");
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
 
+  // Connect to WiFi
   WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nWiFi connected!");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("\nConnected to WiFi!");
 }
 
-// ==================================================================
-//                   MAIN PROGRAM LOOP
-// ==================================================================
 void loop() {
-  // Read data from GPS module
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
+  while (neogps.available() > 0) {
+    if (gps.encode(neogps.read())) {
+      if (gps.location.isValid()) {
+        float latitude = gps.location.lat();
+        float longitude = gps.location.lng();
+
+        Serial.print("Latitude: ");
+        Serial.println(latitude, 6);
+        Serial.print("Longitude: ");
+        Serial.println(longitude, 6);
+
+        // Send to API
+        if (WiFi.status() == WL_CONNECTED) {
+          HTTPClient http;
+          http.begin(serverName);
+
+          http.addHeader("Content-Type", "application/json");
+
+          // Send coordinates as JSON
+          String httpRequestData = "{\"lat\": " + String(latitude, 6) + ", \"lng\": " + String(longitude, 6) + "}";
+          int httpResponseCode = http.POST(httpRequestData);
+
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+
+          http.end();
+        }
+        delay(5000); // send every 5 seconds
+      }
+    }
   }
-
-  // If new location data is available
-  if (gps.location.isUpdated()) {
-    Serial.print("Latitude: ");
-    Serial.println(gps.location.lat(), 6);
-
-    Serial.print("Longitude: ");
-    Serial.println(gps.location.lng(), 6);
-
-    Serial.print("Satellites: ");
-    Serial.println(gps.satellites.value());
-
-    Serial.print("HDOP: ");
-    Serial.println(gps.hdop.value());
-
-    Serial.println("---------------------------");
-  }
-
-  delay(1000); // update every second
-}  
+}
---
+++ sketch_aug28a/sketch_aug28a.ino
@@ -0,0 +1,59 @@
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+// ==================================================================
+//                    HARDWARE CONFIGURATION
+// ==================================================================
+HardwareSerial gpsSerial(1);  
+#define GPS_RX 16             
+#define GPS_TX 17             
+#define GPS_BAUD 9600         
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+TinyGPSPlus gps;  // GPS parser object
+
+// ==================================================================
+//                    INITIALIZATION SETUP
+// ==================================================================
+void setup() {
+  Serial.begin(115200);
+  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, GPS_RX, GPS_TX);
+  Serial.println("Connecting to WiFi...");
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// ==================================================================
+//                   MAIN PROGRAM LOOP
+// ==================================================================
+void loop() {
+  while (gpsSerial.available() > 0) {
+    gps.encode(gpsSerial.read());
+  }
+
+  if (gps.location.isUpdated()) {
+    Serial.print("Latitude: ");
+    Serial.println(gps.location.lat(), 6);
+
+    Serial.print("Longitude: ");
+    Serial.println(gps.location.lng(), 6);
+
+    Serial.print("Satellites: ");
+    Serial.println(gps.satellites.value());
+
+    Serial.print("HDOP: ");
+    Serial.println(gps.hdop.value());
+
+    Serial.println("---------------------------");
+  }
+
+  delay(1000); 
+}  
